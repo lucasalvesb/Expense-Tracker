d4f438d1563b2a66fab88bcf35533d6fa7557ea0
Lucas Alves <lucasalves94@gmail.com>
2023-01-24 15:50:41 -0300

    database migration implementation

--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+
 
 //DI 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
