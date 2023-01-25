e07e6874f0849d2b7c8a1f162028d8ec67280e4c
Lucas Alves <lucasalves94@gmail.com>
2023-01-25 15:41:19 -0300

    textboxes replaced with bootstrap dark and syncfusion

--- Expense Tracker/Controllers/CategoryController.cs
+++ Expense Tracker/Controllers/CategoryController.cs
@@ -45,7 +45,7 @@ namespace Expense_Tracker.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
