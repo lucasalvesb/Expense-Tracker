03676cc452295a9fc649d0d96bf7713b677c0d6a
Lucas Alves <lucasalves94@gmail.com>
2023-01-27 23:36:10 -0300

    added new icon and title property to the table

--- Expense Tracker/Models/Category.cs
+++ Expense Tracker/Models/Category.cs
@@ -14,5 +14,13 @@ namespace Expense_Tracker.Models
         public string Icon { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+                }
     }
 }
