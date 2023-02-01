4f4e52c4f35789cb75b723a1e0b04e801b4bb533
Lucas Alves <lucasalves94@gmail.com>
2023-02-01 14:49:43 -0300

    adding total income, total expense and last 7 days info

--- Expense Tracker/Controllers/DashboardController.cs
+++ Expense Tracker/Controllers/DashboardController.cs
@@ -1,11 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expense_Tracker.Controllers
 {
     public class DashboardController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public DashboardController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            //Last 7 days
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category)
+                .Where(y => y.Date >= StartDate && y.Date <= EndDate)
+                .ToListAsync();
+
+            //Total Income
+
+            int TotalIncome = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+            //Total Expense
+
+            int TotalExpense = SelectedTransactions
+                .Where(i => i.Category.Type == "Income")
+                .Sum(j => j.Amount);
+
+            ViewBag.TotalIncome = TotalIncome.ToString("C0");
+
+
             return View();
         }
     }
