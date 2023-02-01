3abbef06a4b744f8cfe6c011ee05a3acd0aeb9f8
Lucas Alves <lucasalves94@gmail.com>
2023-01-31 21:28:53 -0300

    made the view of transaction correctly

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -18,5 +18,24 @@ namespace Expense_Tracker.Models
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type=="Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
+
     }
 }
